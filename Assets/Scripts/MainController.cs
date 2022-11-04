f3b73cc688bb78a66bbbd8d32b1386041745ff6b
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-11-04 19:39:12 +0500

    fix start conditions

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -21,14 +21,14 @@ public class MainController : MonoBehaviour
     private void Evolution()
     {
         // spawn food
-        for (int i = 0; i < 250; i++)
+        for (int i = 0; i < 100; i++)
         {
             GameObject food = Instantiate(foodPrefab, new Vector3(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y), 0.1f), Quaternion.identity);
             food.name = "food";
         }
 
         // spawn bacteria
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < 15; i++)
         {
             Genome genome = new Genome(64);
             GameObject bacterium = Instantiate(bacteriumPrefab, new Vector3(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y), 0), Quaternion.identity);
@@ -40,10 +40,11 @@ public class MainController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (frame % 1 == 0)
+        if (frame >= 25)
         {
             GameObject food = Instantiate(foodPrefab, new Vector3(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y), 0), Quaternion.identity);
             food.name = "food";
+            frame = 0;
         }
         frame++;
     }
